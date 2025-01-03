5e7755ae0e912965c0a886e12b676fac082f8a6c
jeongeun <ddo_eun@naver.com>
2025-01-03 11:18:17 +0900

    2일차 최종

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,18 +14,23 @@ const Layout = () => {
     <>
       {shouldShowHeader && (
         <header className="flex justify-between items-center p-4 border-b border-gray-200 bg-white mb-10">
-          <img src="/logo.svg" alt="Logo" />
+          <Link to="/">
+            <img src="/logo.svg" alt="Logo" />
+          </Link>
           <div className="flex space-x-4">
             {(() => {
               if (user) {
                 return (
                   <>
-                    <span className="text-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 ">
-                      {user.nickname}
-                    </span>
                     <Link
-                      to="/logout"
-                      className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      to="/mypage"
+                      className="text-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 hover:underline "
+                    >
+                      {user.nickname}
+                    </Link>
+                    <Link
+                      to="/"
+                      className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 "
                     >
                       로그아웃
                     </Link>
@@ -36,13 +41,13 @@ const Layout = () => {
                   <>
                     <Link
                       to="/login"
-                      className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 "
                     >
                       로그인
                     </Link>
                     <Link
                       to="/signup"
-                      className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 "
                     >
                       회원가입
                     </Link>

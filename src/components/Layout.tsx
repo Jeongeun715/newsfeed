1e6a7d3941ef59e64010557e797b42f537bab401
jeongeun <ddo_eun@naver.com>
2025-01-06 12:26:47 +0900

    회원가입 완료

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,63 +1,54 @@
-import { Link } from "react-router-dom";
-import { Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import supabase from "../utils/supabase";
 
 const Layout = () => {
-  const location = useLocation();
-  const hideHeaderPaths = ["/signup", "/login"];
-  const shouldShowHeader = !hideHeaderPaths.includes(location.pathname);
+  // const user = {
+  // 	nickname: "홍길동",
+  // };
+  const user = null;
 
-  const user = {
-    nickname: "쩡으니",
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
   };
 
   return (
     <>
-      {shouldShowHeader && (
-        <header className="flex justify-between items-center p-4 border-b border-gray-200 bg-white mb-10">
-          <Link to="/">
-            <img src="/logo.svg" alt="Logo" />
-          </Link>
-          <div className="flex space-x-4">
-            {(() => {
-              if (user) {
-                return (
-                  <>
-                    <Link
-                      to="/mypage"
-                      className="text-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 hover:underline "
-                    >
-                      {user.nickname}
-                    </Link>
-                    <Link
-                      to="/"
-                      className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 "
-                    >
-                      로그아웃
-                    </Link>
-                  </>
-                );
-              } else {
-                return (
-                  <>
-                    <Link
-                      to="/login"
-                      className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 "
-                    >
-                      로그인
-                    </Link>
-                    <Link
-                      to="/signup"
-                      className="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 "
-                    >
-                      회원가입
-                    </Link>
-                  </>
-                );
-              }
-            })()}
-          </div>
-        </header>
-      )}
+      <header className="flex justify-between bg-white p-4 border-b border-gray-200 items-center">
+        <Link to="/">
+          <img src="/logo.svg" alt="logo" />
+        </Link>
+        <div className="flex gap-2 items-center">
+          {user ? (
+            <>
+              <Link to="/mypage" className="hover:underline">
+                {user.nickname}
+              </Link>
+              <button
+                onClick={handleLogout}
+                className="text-sm text-gray-900 bg-white border border-gray-300 rounded-lg px-5 py-2.5 hover:bg-gray-100 font-medium"
+              >
+                로그아웃
+              </button>
+            </>
+          ) : (
+            <>
+              <Link
+                to="/login"
+                className="text-sm text-gray-900 bg-white border border-gray-300 rounded-lg px-5 py-2.5 hover:bg-gray-100 font-medium"
+              >
+                로그인
+              </Link>
+              <Link
+                to="/signup"
+                className="text-sm text-gray-900 bg-white border border-gray-300 rounded-lg px-5 py-2.5 hover:bg-gray-100 font-medium"
+              >
+                회원가입
+              </Link>
+            </>
+          )}
+        </div>
+      </header>
+
       <Outlet />
     </>
   );

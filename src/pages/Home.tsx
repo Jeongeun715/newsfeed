93fd44b3e8f1c79a6aa678c1dfdbeec94a05596b
jeongeun <ddo_eun@naver.com>
2025-01-08 13:47:51 +0900

    feat/main ui 완료

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="max-w-screen-lg mx-auto min-h-[calc(100vh-100px)] px-10 mb-10">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-2xl font-bold">글 목록</h1>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+            글쓰기
+          </button>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <Link
+            to="/"
+            className="flex justify-between bg-white shadow-md p-6 rounded-lg"
+          >
+            <div>
+              <button className="p-3 bg-gray-100 rounded-lg text-sm flex flex-col items-center gap-1 text-blue-950">
+                👍
+                <div className="font-bold">2</div>
+              </button>
+            </div>
+            <div className="flex-1 px-10 min-w-0 flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <h2 className="text-blue-950 text-xl font-bold">
+                  첫 번째 게시물
+                </h2>
+                <p className="text-gray-600 truncate text-md">123</p>
+              </div>
+              <p className="text-right text-xs text-gray-600">
+                작성일: 2024. 11. 11.
+              </p>
+            </div>
+            <div className="flex items-center gap-1 p-3 text-gray-600">
+              🗨️
+              <div className="font-bold">4</div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
